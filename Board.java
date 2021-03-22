57b05e5582c703b5c2e5f52a0d18b986eeb0e71d
Marilii Saar <ly@marilii.com>
2021-03-22 14:27:33 +0200

    Green to mark active card

--- Board.java
+++ Board.java
@@ -1,28 +1,32 @@
 public class Board {
     private int width;
     private int height;
+    private Player player;    
+    private String colour;
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_GREEN = "\u001B[32m";
 
     public Board(int width, int height) {
         this.width = width;
         this.height = height;
     }
 
-    public void render() {
-        char symbol;
+    public void activePlayer(Player p) {
+        this.player = p;
+    }
 
-        for (int y = 0; y <= height; y++) {
-            for (int x = 0; x <= width; x++) {
-                if (y == 0 || y == height) {
-                    symbol = '-';
-                } else if ( x == 0 || x == width) {
-                    symbol = '|';
-                } else {
-                    symbol = ' ';
+    public void render() {
+        String symbol = "▯ ";
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                colour = ANSI_RESET;
+                if (player != null && player.getX() == x && player.getY() == y) {
+                    colour = ANSI_GREEN;
                 }
-                System.out.print(symbol);
+                System.out.print(colour + symbol + ANSI_RESET);
             }
             System.out.println();
         }
-    }
-    
+    }    
 }
