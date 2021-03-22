bf04943d41af9d548d21291114d983fe75694567
Marilii Saar <ly@marilii.com>
2021-03-22 13:25:38 +0200

    Game board rendering

--- Board.java
+++ Board.java
@@ -6,5 +6,21 @@ public class Board {
         this.width = width;
         this.height = height;
     }
+
+    public void render() {
+        char symbol;
+
+        for (int y = 0; y <= height; y++) {
+            for (int x = 0; x <= width; x++) {
+                if (y == 0 || y == height) {
+                    symbol = '-';
+                } else if ( x == 0 || < == width) {
+                    symbol = '|';
+                } else {
+                    symbol = Character.toChars(9633);
+                }
+            }
+        }
+    }
     
 }
